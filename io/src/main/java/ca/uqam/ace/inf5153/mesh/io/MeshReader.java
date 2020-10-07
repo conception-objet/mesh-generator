8087d7630415f090c0a15bd32e4b0df8da9b33cb
Sébastien Mosser <sebastien.mosser@gmail.com>
2020-10-07 08:32:59 -0400

    maven central & github release compliance

--- io/src/main/java/ca/uqam/ace/inf5153/mesh/io/MeshReader.java
+++ io/src/main/java/ca/uqam/ace/inf5153/mesh/io/MeshReader.java
@@ -5,6 +5,9 @@ import ca.uqam.ace.inf5153.mesh.io.Structs.Mesh;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Read a Mesh
+ */
 public class MeshReader {
 
     public Mesh readFromFile(String fileName) throws IOException {
--- io/src/main/java/ca/uqam/ace/inf5153/mesh/io/MeshWriter.java
+++ io/src/main/java/ca/uqam/ace/inf5153/mesh/io/MeshWriter.java
@@ -2,10 +2,12 @@ package ca.uqam.ace.inf5153.mesh.io;
 
 import ca.uqam.ace.inf5153.mesh.io.Structs.Mesh;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * Write a mesh
+ */
 public class MeshWriter {
 
     public void writeToFile(Mesh m, String fileName) throws IOException {
